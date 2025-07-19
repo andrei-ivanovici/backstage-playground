67f45544d22450cd2bdf05251dbe3c80f5a98875
Andrei Ivanovici <andrei.ivanovici@gmail.com>
2025-07-19 10:23:43 +0300

    feat: add  devtools

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -52,4 +52,6 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
+backend.add(import('@backstage/plugin-devtools-backend'));
+backend.add(import('@backstage/plugin-catalog-backend-module-unprocessed'));
 backend.start();
